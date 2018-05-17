612d930df7117017e0c47f60212b20c1e30a3664
silex5z <silex5z@naver.com>
2018-05-17 11:29:09 +0900

    chapter12

--- javascript-study/chapter11/chapter11-1.js
+++ javascript-study/chapter11/chapter11-1.js
@@ -1,64 +1,62 @@
 function initMap() {
+    // 맵 스타일 객체
+    var styleArray = [
+        {	
+        featureType: 'all',
+        stylers    : [
+            { saturation: -80 }
+        ]
+        },{
+        featureType: 'road.arterial',
+        elementType: 'geometry',
+        stylers    : [
+            { hue: '#00ffee' },
+            { saturation: 50 }
+        ]
+        },{
+        featureType: 'poi.business',
+        elementType: 'labels',
+        stylers    : [
+            { visibility: 'off' }
+        ]
+        }
+    ];      
 
-			// 맵 스타일 객체
-			var styleArray = [
-			  {	
-			    featureType: 'all',
-			    stylers    : [
-			      { saturation: -80 }
-			    ]
-			  },{
-			    featureType: 'road.arterial',
-			    elementType: 'geometry',
-			    stylers    : [
-			      { hue: '#00ffee' },
-			      { saturation: 50 }
-			    ]
-			  },{
-			    featureType: 'poi.business',
-			    elementType: 'labels',
-			    stylers    : [
-			      { visibility: 'off' }
-			    ]
-			  }
-			];      
+    var my_position = {lat: 37.571717, lng: 126.976479};
 
-            var my_position = {lat: 37.571717, lng: 126.976479};
+    // 맵 객체를 생성하고 id=‘map’에 지도 표시
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center     : my_position,
+        scrollwheel: false,
+        zoom       : 17
+    });
 
-			// 맵 객체를 생성하고 id=‘map’에 지도 표시
-			var map = new google.maps.Map(document.getElementById('map'), {
-	        	center     : my_position,
-	        	scrollwheel: false,
-	        	zoom       : 17
-			});
+    var infoWindow = new google.maps.InfoWindow({map: map});
+    
+        if(navigator.geolocation){
+            //지오로케이션을 사용할 수 있는 경우
+            navigator.geolocation.getCurrentPosition(function(position){
+                var pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
+                infoWindow.setPosition(pos);
+                infoWindow.setContent('여기가 내 위치입니다.');
+                map.setCenter(pos);
+            },function(){
+                //연결 실패
+                handleLocationError(true, infoWindow, map.getCenter());
+            });
+        } else {
+            //지오로케이션을 사용할 수 없는 경우
+            handleLocationError(false, infoWindow, map.getCenter());
+        }
+    }	// end initMap();
 
-            var infoWindow = new google.maps.InfoWindow({map: map});
-            
-				if(navigator.geolocation){
-					//지오로케이션을 사용할 수 있는 경우
-					navigator.geolocation.getCurrentPosition(function(position){
-						var pos = {
-							lat: position.coords.latitude,
-							lng: position.coords.longitude
-						};
-						infoWindow.setPosition(pos);
-            			infoWindow.setContent('여기가 내 위치입니다.');
-						map.setCenter(pos);
-					},function(){
-						//연결 실패
-						handleLocationError(true, infoWindow, map.getCenter());
-					});
-				} else {
-					//지오로케이션을 사용할 수 없는 경우
-					handleLocationError(false, infoWindow, map.getCenter());
-				}
-            
-		}	// end initMap();
-
-		//지오로케이션 오류 처리
-		function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-			infoWindow.setPosition(pos);
-			infoWindow.setContent(browserHasGeolocation ?
-			'오류: 지오로케이션 연결 실패': 
-			'오류: 브라우저에서 지오로케이션을 지원하지 않음');
-		}
+    //지오로케이션 오류 처리
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+        '오류: 지오로케이션 연결 실패': 
+        '오류: 브라우저에서 지오로케이션을 지원하지 않음');
+    }
---
+++ javascript-study/chapter12/chapter12.js
@@ -0,0 +1,69 @@
+//API 요청 URL 변수
+var url = 'http://api.openweathermap.org/data/2.5/weather?q=seoul,kr&APPID=ff09a15d02d683d98e0fc482ffed1a29';
+
+// 로딩 이미지 표시
+$('#weather-info .load-img').show();
+initWidget(url);
+
+function initWidget(url){
+    console.log(url);
+    $.getJSON(url, function(data){
+        // 날씨 data 객체
+        var sys     = data.sys;
+        var city    = data.name;
+        var weather = data.weather;
+        var main    = data.main;
+
+        var wmain   = weather[0].main;
+        var wId     = weather[0].id;
+        var icon    = weather[0].icon;
+        var country = sys.country;
+        var temp    = main.temp;
+        var tempMin = main.temp_min;
+        var tempMax = main.temp_max;
+
+        // 날씨 아이콘 표시
+        var iconUrl = 'http://openweathermap.org/img/w/' + icon;
+
+        // 날씨 정보 표시
+        $('#weather-info > .city').html(city + "/" + country);
+        $('#weather-info .icon').html("<img src ='" + iconUrl + ".png'>");
+        $('#weather-info .w-id').html(wmain);
+        $('#weather-info .temp-min').html(parseInt(tempMin-273.15) + '&deg' + ' min');
+        $('#weather-info .temp-max').html(parseInt(tempMax-273.15) + '&deg' + ' max');
+        $('#weather-info .temp').html(parseInt(temp-273.15) + '&deg');
+
+        // 데이터 로딩 후 로딩 이미지 제거
+        $('#weather-info .load-img').hide();
+
+    }).fail(function(){
+        alert('loading error');
+    })
+}
+
+function getGeolocation() {
+    if(navigator.geolocation){
+        //지오로케이션을 사용할 수 있는 경우
+        navigator.geolocation.getCurrentPosition(function(position){
+            var lat = position.coords.latitude;
+            var lng = position.coords.longitude;
+                url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat +
+            '&lon=' + lng + '&APPID=ff09a15d02d683d98e0fc482ffed1a29';
+            initWidget(url);
+        },function(){
+            //연결 실패
+            handleLocationError(true);
+            });
+    } else {
+        //지오로케이션을 사용할 수 없는 경우
+        handleLocationError(false);
+    }
+}
+
+function handleLocationError(browserHasGeolocation) {
+    initWidget(url);
+    alert(browserHasGeolocation ?     
+    '오류: 지오로케이션 연결 실패': 
+    '오류: 브라우저에서 지오로케이션을 지원하지 않음');
+}
+
